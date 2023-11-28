70a14970d233225ffca29d02aaa354f55743f9cd
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2023-11-28 13:43:00 -0700

    added backend url

--- client/src/config/apiUrls.js
+++ client/src/config/apiUrls.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://netflix-clone-api-7fi7.onrender.com';
 
 const CONTENT_URLS = {
   HORROR: `${API_BASE_URL}/content/discover/horror`,
